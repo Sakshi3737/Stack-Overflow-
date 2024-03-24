010f8a0d5b638212a0113e0f1057b98f96598b4c
Sakshi3737 <134384945+Sakshi3737@users.noreply.github.com>
2024-03-24 21:27:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import axios from "axios";
 
-import userRoutes from "./routes/users.js";
-import questionRoutes from "./routes/Questions.js";
-import answerRoutes from "./routes/Answers.js";
-
-const app = express();
-dotenv.config();
-
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("This is a stack overflow clone API");
+const API = axios.create({
+  baseURL: "https://stack-overflow-flkp.onrender.com",
+  // http://localhost:5000
 });
 
-app.use("/user", userRoutes);
-app.use("/questions", questionRoutes);
-app.use("/answer", answerRoutes);
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("Profile")) {
+    req.headers.authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("Profile")).token
+    }`;
+  }
+  return req;
+});
+export const logIn = (authData) => API.post("/user/login", authData);
+export const signUp = (authData) => API.post("/user/signup", authData);
 
-const PORT = process.env.PORT || 5000;
+export const postQues = (quesData) => API.post("/questions/Ask", quesData);
+export const getAllQuestions = () => API.get("/questions/get");
+export const deleteQuestion = (id) => API.delete(`/questions/delete/${id}`);
+export const voteQues = (id, value, userId) =>
+  API.patch(`/questions/vote/${id}`, { value, userId });
 
-const DB_URL = process.env.CONNECTION_URL;
+export const postAns = (id, noOfAns, answerBody, userAns, userId) =>
+  API.patch(`/answer/post/${id}`, { noOfAns, answerBody, userAns, userId });
 
-mongoose
-  .connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log(`server running on port ${PORT}`);
-    })
-  )
-  .catch((err) => console.log(err.message));
+export const deleteAnswer = (id, answerId, noOfAns, userAns) =>
+  API.patch(`/answer/delete/${id}`, { answerId, noOfAns, userAns });
+
+export const getAllUsers = () => API.get("/user/getAllUsers");
+export const updateProfile = (id, updateData) =>
+  API.patch(`/user/update/${id}`, updateData);
